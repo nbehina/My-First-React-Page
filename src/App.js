ba235c2ea1642f94b1670a625ec191bc98b230bb
Your Namenbehina <nbehina@gmail.com>
2020-04-07 20:10:50 -0600

    My First React Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Image } from 'semantic-ui-react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <h1>My First React Page</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +20,23 @@ function App() {
         >
           Learn React
         </a>
+        <form class="ui form">
+  <div class="field">
+    <label>First Name</label>
+    <input placeholder="First Name" />
+  </div>
+  <div class="field">
+    <label>Last Name</label>
+    <input placeholder="Last Name" />
+  </div>
+  <div class="field">
+    <div class="ui checkbox">
+      <input type="checkbox" class="hidden" readonly="" tabindex="0" />
+      <label>I agree to the Terms and Conditions</label>
+    </div>
+  </div>
+  <button type="submit" class="ui button">Submit</button>
+</form>
       </header>
     </div>
   );
